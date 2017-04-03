0e12c11df004caf85b6205d9ee666bf9ad84e8d3
Mark Riechers <markriechers@gmail.com>
2017-04-03 16:07:50 -0500

    Missed a comma and all hell broke loose

--- settings/index.js
+++ settings/index.js
@@ -28,7 +28,7 @@ module.exports = {
   { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-Light.ttf"), weight: 300 },
   { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-Bold.ttf"), weight: "bold" },
   { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-Italic.ttf"), style: "italic" },
-  { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-BoldItalic.ttf"), weight: "bold", style: "italic" }
+  { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-BoldItalic.ttf"), weight: "bold", style: "italic" },
   { family: "Proxima Nova", file: path.join(__dirname, "fonts", "ProximaNova-Reg-webfont.ttf") },
   { family: "Proxima Nova", file: path.join(__dirname, "fonts", "ProximaNova-Light-webfont.ttf"), weight: 300 },
   { family: "Proxima Nova", file: path.join(__dirname, "fonts", "ProximaNova-Bold-webfont.ttf"), weight: "bold" },
